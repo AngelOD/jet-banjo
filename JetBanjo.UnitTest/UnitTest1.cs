a1286451a702b3f20057cfa43255f0782e36aae4
Crimix <christofferm@live.dk>
2018-04-13 15:02:38 +0200

    - Added custom image - Starting to add more unit tests

--- JetBanjo.UnitTest/UnitTest1.cs
+++ JetBanjo.UnitTest/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xamarin.Forms;
+using JetBanjo.Utils;
 
 namespace JetBanjo.UnitTest
 {
@@ -9,6 +13,14 @@ namespace JetBanjo.UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            List<CImage> l1 = new List<CImage>();
+            l1.Add(new CImage("JetBanjo.Resources.error.png"));
+            l1.Add(new CImage("JetBanjo.Resources.badair.png"));
+            List<CImage> l2 = new List<CImage>();
+            l2.Add(new CImage("JetBanjo.Resources.error.png"));
+            l2.Add(new CImage("JetBanjo.Resources.badair.png"));
+            
+            Assert.IsTrue(l1.SequenceEqual(l2));
         }
     }
 }
