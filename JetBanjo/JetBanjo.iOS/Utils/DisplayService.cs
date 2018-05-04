0f6b41e9201ac2fd761d58f296aac32d5a1eb75d
Unknown <Tkraus13@student.aau.dk>
2018-05-04 15:34:58 +0200

    Dismiss sync added

--- JetBanjo/JetBanjo.iOS/Utils/DisplayService.cs
+++ JetBanjo/JetBanjo.iOS/Utils/DisplayService.cs
@@ -49,7 +49,6 @@ namespace JetBanjo.iOS.Utils
         }
         private UIImage GetImageFromImageSourceAsync(ImageSource imageSource)
         {
-            Console.WriteLine("Test 2");
             IImageSourceHandler handler = null;
 
             if (imageSource is FileImageSource)
@@ -75,112 +74,140 @@ namespace JetBanjo.iOS.Utils
         }
         public void ShowDialog(string title, string text, ImageSource source)
         {
-            Console.WriteLine("Test 3");
             ShowDialog(title, text, source, null);
         }
         public void ShowDialog(string title, string text, Action callback)
         {
-            Console.WriteLine("Test 4");
             ShowDialog(title, text, null, callback);
         }
 
         public void ShowDialog(string title, string text)
         {
-            Console.WriteLine("Test 5");
             ShowDialog(title, text, null, null);
         }
         public void DismissActivityIndicator()
         {
-            Console.WriteLine("Test 6");
             dialog?.DismissViewController(true, () => { });
         }
 
         public void DismissDialog()
         {
-            Console.WriteLine("Test 7");
             dialog?.DismissViewController(true, () => { });
         }
 
         public void DismissInputDialog()
         {
-            Console.WriteLine("Test 8");
             dialog?.DismissViewController(true, () => { });
         }
-
         public void ShowActivityIndicator()
         {
-            Console.WriteLine("Test 9");
-            dialog = UIAlertController.Create("", "", UIAlertControllerStyle.Alert);
-            dialog.View.Opaque = false;
-            dialog.View.Alpha = 0;
-            activityIndicator = new UIActivityIndicatorView(dialog.View.Bounds);
-            activityIndicator.AutoresizingMask = UIViewAutoresizing.All;
-            activityIndicator.ActivityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray;
-            activityIndicator.StartAnimating();
-            dialog.View.AddSubview(activityIndicator);
-            UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(dialog, true, () => { });
+            void Display()
+            {
+                dialog = UIAlertController.Create("", "", UIAlertControllerStyle.Alert);
+                dialog.View.Opaque = false;
+                dialog.View.Alpha = 0;
+                activityIndicator = new UIActivityIndicatorView(dialog.View.Bounds);
+                activityIndicator.AutoresizingMask = UIViewAutoresizing.All;
+                activityIndicator.ActivityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray;
+                activityIndicator.StartAnimating();
+                dialog.View.AddSubview(activityIndicator);
+                UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(dialog, true, () => { });
+            }
+            if (dialog != null)
+            {
+                dialog.DismissViewController(true, () =>
+                {
+                    Display();
+                });
+            }
+            else
+            {
+                Display();
+            }
         }
         public void ShowInputDialog(string title, string text, string ok, string hint, Action<string> callback)
         {
-            Console.WriteLine("Test 10");
             ShowInputDialog(title, text, ok, null, hint, null, (b,s) => { callback(s); });
         }
 
         public void ShowInputDialog(string title, string text, string ok, string hint, string placeholder, Action<string> callback)
         {
-            Console.WriteLine("Test 11");
             ShowInputDialog(title, text, ok, null, hint, placeholder, (b, s) => { callback(s); });
 
         }
 
         public void ShowInputDialog(string title, string text, string ok, string cancel, string hint, Action<bool, string> callback)
         {
-            Console.WriteLine("Test 12");
             ShowInputDialog(title, text, ok, cancel, hint, null, callback);
         }
-
         public void ShowInputDialog(string title, string text, string ok, string cancel, string hint, string placeholder, Action<bool, string> callback)
         {
-            Console.WriteLine("Test 13");
-            dialog = UIAlertController.Create(title, text, UIAlertControllerStyle.Alert);
-            UITextField input = null;
-
-            dialog.AddTextField((textField) =>
+            void Display()
             {
-                input = textField;
+                dialog = UIAlertController.Create(title, text, UIAlertControllerStyle.Alert);
+                UITextField input = null;
 
-                input.Placeholder = hint;
-                if (placeholder != null)
-                    input.Text = placeholder;
-                input.AutocorrectionType = UITextAutocorrectionType.No;
-                input.KeyboardType = UIKeyboardType.Default;
-                input.ReturnKeyType = UIReturnKeyType.Done;
-                input.ClearButtonMode = UITextFieldViewMode.WhileEditing;
-            });
-            dialog.AddAction(UIAlertAction.Create(ok, UIAlertActionStyle.Default, (x) =>
-            {
-                callback(true,input.Text);
-            }));
-            if(cancel != null)
-                dialog.AddAction(UIAlertAction.Create(cancel, UIAlertActionStyle.Cancel, (x) =>
+                dialog.AddTextField((textField) =>
                 {
-                    callback(false, input.Text);
+                    input = textField;
+
+                    input.Placeholder = hint;
+                    if (placeholder != null)
+                        input.Text = placeholder;
+                    input.AutocorrectionType = UITextAutocorrectionType.No;
+                    input.KeyboardType = UIKeyboardType.Default;
+                    input.ReturnKeyType = UIReturnKeyType.Done;
+                    input.ClearButtonMode = UITextFieldViewMode.WhileEditing;
+                });
+                dialog.AddAction(UIAlertAction.Create(ok, UIAlertActionStyle.Default, (x) =>
+                {
+                    callback(true, input.Text);
                 }));
-            UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(dialog, true, () => { });
+                if (cancel != null)
+                    dialog.AddAction(UIAlertAction.Create(cancel, UIAlertActionStyle.Cancel, (x) =>
+                    {
+                        callback(false, input.Text);
+                    }));
+                UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(dialog, true, () => { });
+            }
+            if (dialog != null)
+            {
+                dialog.DismissViewController(true, () =>
+                {
+                    Display();
+                });
+            }
+            else
+            {
+                Display();
+            }
         }
 
         public void ShowToast(string text, bool isLong)
         {
-            Console.WriteLine("Test 14");
-            double seconds = 2.0;
-            if (isLong)
-                seconds = 3.5;
+            void Display()
+            {
+                double seconds = 2.0;
+                if (isLong)
+                    seconds = 3.5;
 
                 var alertDelay = NSTimer.CreateScheduledTimer(seconds, (obj) => {
-                    dialog.DismissViewController(true,()=> { });
-                    });
-            dialog = UIAlertController.Create(null, text, UIAlertControllerStyle.Alert);
+                    dialog.DismissViewController(true, () => { });
+                });
+                dialog = UIAlertController.Create(null, text, UIAlertControllerStyle.Alert);
                 UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(dialog, true, null);
+            }
+            if (dialog != null)
+            {
+                dialog.DismissViewController(true, () =>
+                {
+                    Display();
+                });
+            }
+            else
+            {
+                Display();
+            }
         }
     }
 }
