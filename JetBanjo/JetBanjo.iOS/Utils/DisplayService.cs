73f1ee78634505fac2c7b4368cb07be7b0af738f
Unknown <Tkraus13@student.aau.dk>
2018-05-04 14:19:47 +0200

    DonF Fix

--- JetBanjo/JetBanjo.iOS/Utils/DisplayService.cs
+++ JetBanjo/JetBanjo.iOS/Utils/DisplayService.cs
@@ -18,23 +18,34 @@ namespace JetBanjo.iOS.Utils
     {
 
         private static UIAlertController dialog;
-        private static UIAlertController activitydialog;
         private static UIActivityIndicatorView activityIndicator;
-        private static UIAlertController inputDialog;
-        private static UIAlertController toast;
 
         public void ShowDialog(string title, string text, ImageSource source, Action callback)
         {
-            Console.WriteLine("Test 1");
-            dialog = UIAlertController.Create(title, text, UIAlertControllerStyle.Alert); //Creates a AlertController from the title and text
-            if (source != null) //If the image source is not null, load the image and adds it to the controller
-                dialog.View.AddSubview(new UIImageView(GetImageFromImageSourceAsync(source)));
+            void Display()
+            {
+                dialog = UIAlertController.Create(title, text, UIAlertControllerStyle.Alert); //Creates a AlertController from the title and text
+                if (source != null) //If the image source is not null, load the image and adds it to the controller
+                    dialog.View.AddSubview(new UIImageView(GetImageFromImageSourceAsync(source)));
                 UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(dialog, true, () => { }); //Show the dialog
-            if (callback != null)   
-            { //If the callback is not null, we create an ok button, when pressed execute the callback
-                var action = UIAlertAction.Create(Translator.Translate("ok"), UIAlertActionStyle.Default, ((a) => { callback(); }));
-                dialog.AddAction(action); //Add it to the controller
+                if (callback != null)
+                { //If the callback is not null, we create an ok button, when pressed execute the callback
+                    var action = UIAlertAction.Create(Translator.Translate("ok"), UIAlertActionStyle.Default, ((a) => { callback(); }));
+                    dialog.AddAction(action); //Add it to the controller
+                }
             }
+            if (dialog != null)
+            {
+                dialog.DismissViewController(true, () =>
+                {
+                    Display();
+                });
+            }
+            else
+            {
+                Display();
+            }
+
         }
         private UIImage GetImageFromImageSourceAsync(ImageSource imageSource)
         {
@@ -81,7 +92,7 @@ namespace JetBanjo.iOS.Utils
         public void DismissActivityIndicator()
         {
             Console.WriteLine("Test 6");
-            activitydialog?.DismissViewController(true, () => { });
+            dialog?.DismissViewController(true, () => { });
         }
 
         public void DismissDialog()
@@ -93,21 +104,21 @@ namespace JetBanjo.iOS.Utils
         public void DismissInputDialog()
         {
             Console.WriteLine("Test 8");
-            inputDialog?.DismissViewController(true, () => { });
+            dialog?.DismissViewController(true, () => { });
         }
 
         public void ShowActivityIndicator()
         {
             Console.WriteLine("Test 9");
-            activitydialog = UIAlertController.Create("", "", UIAlertControllerStyle.Alert);
-            activitydialog.View.Opaque = false;
-            activitydialog.View.Alpha = 0;
-            activityIndicator = new UIActivityIndicatorView(activitydialog.View.Bounds);
+            dialog = UIAlertController.Create("", "", UIAlertControllerStyle.Alert);
+            dialog.View.Opaque = false;
+            dialog.View.Alpha = 0;
+            activityIndicator = new UIActivityIndicatorView(dialog.View.Bounds);
             activityIndicator.AutoresizingMask = UIViewAutoresizing.All;
             activityIndicator.ActivityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray;
             activityIndicator.StartAnimating();
-            activitydialog.View.AddSubview(activityIndicator);
-            UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(activitydialog, true, () => { });
+            dialog.View.AddSubview(activityIndicator);
+            UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(dialog, true, () => { });
         }
         public void ShowInputDialog(string title, string text, string ok, string hint, Action<string> callback)
         {
@@ -131,10 +142,10 @@ namespace JetBanjo.iOS.Utils
         public void ShowInputDialog(string title, string text, string ok, string cancel, string hint, string placeholder, Action<bool, string> callback)
         {
             Console.WriteLine("Test 13");
-            inputDialog = UIAlertController.Create(title, text, UIAlertControllerStyle.Alert);
+            dialog = UIAlertController.Create(title, text, UIAlertControllerStyle.Alert);
             UITextField input = null;
 
-            inputDialog.AddTextField((textField) =>
+            dialog.AddTextField((textField) =>
             {
                 input = textField;
 
@@ -146,16 +157,16 @@ namespace JetBanjo.iOS.Utils
                 input.ReturnKeyType = UIReturnKeyType.Done;
                 input.ClearButtonMode = UITextFieldViewMode.WhileEditing;
             });
-            inputDialog.AddAction(UIAlertAction.Create(ok, UIAlertActionStyle.Default, (x) =>
+            dialog.AddAction(UIAlertAction.Create(ok, UIAlertActionStyle.Default, (x) =>
             {
                 callback(true,input.Text);
             }));
             if(cancel != null)
-                inputDialog.AddAction(UIAlertAction.Create(cancel, UIAlertActionStyle.Cancel, (x) =>
+                dialog.AddAction(UIAlertAction.Create(cancel, UIAlertActionStyle.Cancel, (x) =>
                 {
                     callback(false, input.Text);
                 }));
-            UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(inputDialog, true, () => { });
+            UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(dialog, true, () => { });
         }
 
         public void ShowToast(string text, bool isLong)
@@ -166,10 +177,10 @@ namespace JetBanjo.iOS.Utils
                 seconds = 3.5;
 
                 var alertDelay = NSTimer.CreateScheduledTimer(seconds, (obj) => {
-                    toast.DismissViewController(true,()=> { });
+                    dialog.DismissViewController(true,()=> { });
                     });
-                toast = UIAlertController.Create(null, text, UIAlertControllerStyle.Alert);
-                UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(toast, true, null);
+            dialog = UIAlertController.Create(null, text, UIAlertControllerStyle.Alert);
+                UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(dialog, true, null);
         }
     }
 }
