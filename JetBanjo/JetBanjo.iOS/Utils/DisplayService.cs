4d09734af99e1bd320a8ebe9dc4617c6eb374228
tkraus13 <tkraus13@student.aau.dk>
2018-05-07 12:47:01 +0200

    - Update nugets
    - Made it work

--- JetBanjo/JetBanjo.iOS/Utils/DisplayService.cs
+++ JetBanjo/JetBanjo.iOS/Utils/DisplayService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using Foundation;
 using JetBanjo.iOS.Utils;
 using JetBanjo.Utils;
@@ -18,39 +19,128 @@ namespace JetBanjo.iOS.Utils
     {
 
         private static UIAlertController dialog;
+        private static UIAlertController inputDialog;
         private static UIActivityIndicatorView activityIndicator;
+        private static UIAlertController activityDialog;
+        private static UIAlertController toast;
+
+        public void ShowDialog(string title, string text)
+        {
+            ShowDialog(title, text, null, null);
+        }
+
+        public void ShowDialog(string title, string text, ImageSource source)
+        {
+            ShowDialog(title, text, source, null);
+        }
+
+        public void ShowDialog(string title, string text, Action callback)
+        {
+            ShowDialog(title, text, null, callback);
+        }
 
         public void ShowDialog(string title, string text, ImageSource source, Action callback)
         {
-            Console.WriteLine("Test 1");
-            void Display()
-            {
-                dialog = UIAlertController.Create(title, text, UIAlertControllerStyle.Alert); //Creates a AlertController from the title and text
-                if (source != null) //If the image source is not null, load the image and adds it to the controller
-                    dialog.View.AddSubview(new UIImageView(GetImageFromImageSourceAsync(source)));
-                UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(dialog, true, () => { }); //Show the dialog
-                if (callback != null)
-                { //If the callback is not null, we create an ok button, when pressed execute the callback
-                    var action = UIAlertAction.Create(Translator.Translate("ok"), UIAlertActionStyle.Default, ((a) => { callback(); }));
-                    dialog.AddAction(action); //Add it to the controller
-                }
+            dialog = UIAlertController.Create(title, text, UIAlertControllerStyle.Alert); //Creates a AlertController from the title and text
+            if (source != null) //If the image source is not null, load the image and adds it to the controller
+                dialog.View.AddSubview(new UIImageView(GetImageFromImageSourceAsync(source)));
+            if (callback != null)
+            { //If the callback is not null, we create an ok button, when pressed execute the callback
+                var action = UIAlertAction.Create(Translator.Translate("ok"), UIAlertActionStyle.Default, ((a) => { callback(); }));
+                dialog.AddAction(action); //Add it to the controller
             }
-            if (dialog != null)
-            {
-                dialog.DismissViewController(true, () =>
-                {
-                    Display();
-                });
-            }
-            else
-            {
-                Display();
-            }
-
+            GetTopViewController().PresentViewController(dialog, true, null); //Show the dialog
         }
+
+        public void DismissDialog()
+        {
+            dialog?.DismissViewController(true, null);
+        }
+
+        public void ShowActivityIndicator()
+        {
+            activityDialog = UIAlertController.Create("", "", UIAlertControllerStyle.Alert);
+            activityDialog.View.Opaque = false;
+            activityDialog.View.Alpha = 0;
+            activityIndicator = new UIActivityIndicatorView(activityDialog.View.Bounds);
+            activityIndicator.AutoresizingMask = UIViewAutoresizing.All;
+            activityIndicator.ActivityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray;
+            activityIndicator.StartAnimating();
+            activityDialog.View.AddSubview(activityIndicator);
+            GetTopViewController().PresentViewController(activityDialog, true, null);
+        }
+
+        public void DismissActivityIndicator()
+        {
+            activityDialog?.DismissViewController(true, null);
+        }
+
+        public void ShowInputDialog(string title, string text, string ok, string hint, Action<string> callback)
+        {
+            ShowInputDialog(title, text, ok, null, hint, null, (b, s) => { callback(s); });
+        }
+
+        public void ShowInputDialog(string title, string text, string ok, string hint, string placeholder, Action<string> callback)
+        {
+            ShowInputDialog(title, text, ok, null, hint, placeholder, (b, s) => { callback(s); });
+        }
+
+        public void ShowInputDialog(string title, string text, string ok, string cancel, string hint, Action<bool, string> callback)
+        {
+            ShowInputDialog(title, text, ok, cancel, hint, null, callback);
+        }
+
+        public void ShowInputDialog(string title, string text, string ok, string cancel, string hint, string placeholder, Action<bool, string> callback)
+        {
+            inputDialog = UIAlertController.Create(title, text, UIAlertControllerStyle.Alert);
+            UITextField input = null;
+
+            inputDialog.AddTextField((textField) =>
+                {
+                    input = textField;
+
+                    input.Placeholder = hint;
+                    if (placeholder != null)
+                        input.Text = placeholder;
+                    input.AutocorrectionType = UITextAutocorrectionType.No;
+                    input.KeyboardType = UIKeyboardType.Default;
+                    input.ReturnKeyType = UIReturnKeyType.Done;
+                    input.ClearButtonMode = UITextFieldViewMode.WhileEditing;
+                });
+            inputDialog.AddAction(UIAlertAction.Create(ok, UIAlertActionStyle.Default, (x) =>
+                {
+                    callback(true, input.Text);
+                }));
+            if (cancel != null)
+                inputDialog.AddAction(UIAlertAction.Create(cancel, UIAlertActionStyle.Cancel, (x) =>
+                    {
+                        callback(false, input.Text);
+                    }));
+            GetTopViewController().PresentViewController(inputDialog, true, null);
+        }
+
+        public void DismissInputDialog()
+        {
+            inputDialog?.DismissViewController(true, null);
+        }
+
+        public void ShowToast(string text, bool isLong)
+        {
+            double seconds = 2.0;
+            if (isLong)
+                seconds = 3.5;
+
+            var alertDelay = NSTimer.CreateScheduledTimer(seconds, (obj) =>
+            {
+                toast.DismissViewController(true, null);
+            });
+            toast = UIAlertController.Create(null, text, UIAlertControllerStyle.Alert);
+            GetTopViewController().PresentViewController(toast, true, null);
+        }
+
+
         private UIImage GetImageFromImageSourceAsync(ImageSource imageSource)
         {
-            Console.WriteLine("Test 2");
             IImageSourceHandler handler = null;
 
             if (imageSource is FileImageSource)
@@ -74,153 +164,14 @@ namespace JetBanjo.iOS.Utils
 
             return image;
         }
-        public void ShowDialog(string title, string text, ImageSource source)
-        {
-            Console.WriteLine("Test 3");
-            ShowDialog(title, text, source, null);
-        }
-        public void ShowDialog(string title, string text, Action callback)
-        {
-            Console.WriteLine("Test 4");
-            ShowDialog(title, text, null, callback);
-        }
 
-        public void ShowDialog(string title, string text)
+        private static UIViewController GetTopViewController()
         {
-            Console.WriteLine("Test 5");
-            ShowDialog(title, text, null, null);
+            var vc = UIApplication.SharedApplication.KeyWindow.RootViewController;
+            while (vc.PresentedViewController != null)
+                vc = vc.PresentedViewController;
+            return vc;
         }
-        public void DismissActivityIndicator()
-        {
-            Console.WriteLine("Test 6");
-            dialog?.DismissViewController(true, () => { });
-        }
-
-        public void DismissDialog()
-        {
-            Console.WriteLine("Test 7");
-            dialog?.DismissViewController(true, () => { });
-        }
-
-        public void DismissInputDialog()
-        {
-            Console.WriteLine("Test 8");
-            dialog?.DismissViewController(true, () => { });
-        }
-        public void ShowActivityIndicator()
-        {
-            Console.WriteLine("Test 9");
-            void Display()
-            {
-                dialog = UIAlertController.Create("", "", UIAlertControllerStyle.Alert);
-                dialog.View.Opaque = false;
-                dialog.View.Alpha = 0;
-                activityIndicator = new UIActivityIndicatorView(dialog.View.Bounds);
-                activityIndicator.AutoresizingMask = UIViewAutoresizing.All;
-                activityIndicator.ActivityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray;
-                activityIndicator.StartAnimating();
-                dialog.View.AddSubview(activityIndicator);
-                UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(dialog, true, () => { });
-            }
-            if (dialog != null)
-            {
-                dialog.DismissViewController(true, () =>
-                {
-                    Display();
-                });
-            }
-            else
-            {
-                Display();
-            }
-        }
-        public void ShowInputDialog(string title, string text, string ok, string hint, Action<string> callback)
-        {
-            Console.WriteLine("Test 10");
-            ShowInputDialog(title, text, ok, null, hint, null, (b,s) => { callback(s); });
-        }
-
-        public void ShowInputDialog(string title, string text, string ok, string hint, string placeholder, Action<string> callback)
-        {
-            Console.WriteLine("Test 11");
-            ShowInputDialog(title, text, ok, null, hint, placeholder, (b, s) => { callback(s); });
-        }
-
-        public void ShowInputDialog(string title, string text, string ok, string cancel, string hint, Action<bool, string> callback)
-        {
-            Console.WriteLine("Test 12");
-            ShowInputDialog(title, text, ok, cancel, hint, null, callback);
-        }
-        public void ShowInputDialog(string title, string text, string ok, string cancel, string hint, string placeholder, Action<bool, string> callback)
-        {
-            Console.WriteLine("Test 13");
-            void Display()
-            {
-                dialog = UIAlertController.Create(title, text, UIAlertControllerStyle.Alert);
-                UITextField input = null;
-
-                dialog.AddTextField((textField) =>
-                {
-                    input = textField;
-
-                    input.Placeholder = hint;
-                    if (placeholder != null)
-                        input.Text = placeholder;
-                    input.AutocorrectionType = UITextAutocorrectionType.No;
-                    input.KeyboardType = UIKeyboardType.Default;
-                    input.ReturnKeyType = UIReturnKeyType.Done;
-                    input.ClearButtonMode = UITextFieldViewMode.WhileEditing;
-                });
-                dialog.AddAction(UIAlertAction.Create(ok, UIAlertActionStyle.Default, (x) =>
-                {
-                    callback(true, input.Text);
-                }));
-                if (cancel != null)
-                    dialog.AddAction(UIAlertAction.Create(cancel, UIAlertActionStyle.Cancel, (x) =>
-                    {
-                        callback(false, input.Text);
-                    }));
-                UIApplication.SharedApplication.Windows[0].RootViewController.PresentViewController(dialog, true, () => { });
-            }
-            if (dialog != null)
-            {
-                dialog.DismissViewController(true, () =>
-                {
-                    Display();
-                });
-            }
-            else
-            {
-                Display();
-            }
-        }
-
-        public void ShowToast(string text, bool isLong)
-        {
-            Console.WriteLine("Test 14");
-            void Display()
-            {
-                double seconds = 2.0;
-                if (isLong)
-                    seconds = 3.5;
-
-                var alertDelay = NSTimer.CreateScheduledTimer(seconds, (obj) => {
-                    dialog.DismissViewController(true, () => { });
-                });
-                dialog = UIAlertController.Create(null, text, UIAlertControllerStyle.Alert);
-                UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(dialog, true, null);
-            }
-            if (dialog != null)
-            {
-                dialog.DismissViewController(true, () =>
-                {
-                    Display();
-                });
-            }
-            else
-            {
-                Display();
-            }
-        }
+            
     }
 }
