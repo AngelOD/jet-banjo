ed3066c4ae61104c8d9dae88da456082746c7bf8
Crimix <christofferm@live.dk>
2018-05-08 09:38:38 +0200

    - Change the avatarUpdateTime to be 5 minutes

--- JetBanjo/JetBanjo/Utils/Constants.cs
+++ JetBanjo/JetBanjo/Utils/Constants.cs
@@ -17,7 +17,7 @@ namespace JetBanjo.Utils
 
         public static TimeSpan cacheMaxAge = new TimeSpan(0, 2, 0); //0 hours, 2 minutes, 0 seconds
         public static TimeSpan timeoutTime = new TimeSpan(0, 0, 30); //0 hours, 0 minutes, 30 seconds
-        public static TimeSpan avatarUpdateTime = new TimeSpan(0, 0, 2); //0 hours, 5 minutes, 0 seconds
+        public static TimeSpan avatarUpdateTime = new TimeSpan(0, 5, 0); //0 hours, 5 minutes, 0 seconds
 
         public const string NETWORK_SEARCH = "_services._dns-sd._udp"; //"_lora_server._tcp";
         public const string API_ROOMS_URL = "/api/rooms";
